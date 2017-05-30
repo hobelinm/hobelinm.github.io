20408d566ed608c5b6b263bb0ce05b208a1c8091
Hugo Belin <info@hugobelin.com>
2017-05-29 22:39:10 -0700

    v0.1.2.0 - Draft custom icons

--- belin-app/src/app/footer/footer.component.ts
+++ belin-app/src/app/footer/footer.component.ts
@@ -12,6 +12,15 @@ export class FooterComponent implements OnInit {
   public myViews : number;
   public siteViews : number;
   public siteVersion : string;
+  public tableauPublicUri : string;
+  public tableauPublicAlt : string;
+  public tableauProfile : string;
+  public xboxUri : string;
+  public xboxAlt : string;
+  public psnUri : string;
+  public psnAlt : string;
+  public nUri : string;
+  public nAlt : string;
 
   constructor() {
     this.year = (new Date).getFullYear().toString();
@@ -19,7 +28,16 @@ export class FooterComponent implements OnInit {
     this.visitors = 0;
     this.myViews = 0;
     this.siteViews = 0;
-    this.siteVersion = "0.1.1.0"
+    this.siteVersion = "0.1.2.0";
+    this.tableauPublicUri = "https://belino.blob.core.windows.net/hugobelinpublic/assets/img/tableau.png";
+    this.tableauPublicAlt = "Tableau Public"; // TODO: Localize this, resource manager
+    this.tableauProfile = "https://public.tableau.com/profile/hugo.belin";
+    this.xboxUri = "https://belino.blob.core.windows.net/hugobelinpublic/assets/img/xbox.png";
+    this.xboxAlt = "Xbox Profile";
+    this.psnUri = "https://belino.blob.core.windows.net/hugobelinpublic/assets/img/psn.png";
+    this.psnAlt = "PlayStation Network";
+    this.nUri = "https://belino.blob.core.windows.net/hugobelinpublic/assets/img/nintendo.png";
+    this.nAlt = "Nintendo Account";
    }
 
   ngOnInit() {
