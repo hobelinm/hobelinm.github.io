738b88c2695fb8bc2652982a64b2c30faacef4b0
Hugo Belin <info@hugobelin.com>
2017-06-04 16:44:08 -0700

    0.1.6.0 - Minor improvements to change log page

--- belin-app/src/app/change-log/change-log.component.ts
+++ belin-app/src/app/change-log/change-log.component.ts
@@ -8,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class ChangeLogComponent implements OnInit {
   public title : string;
   public description : string;
+  public revisionPolicy : string;
+  public buildPolicy : string;
+  public minorPolicy : string;
+  public majorPolicy : string;
+  public clientChangeLog : string;
+  public serverChangeLog : string;
 
   constructor() { 
     this.title = "Change Log";
-    this.description = "A list of the changes made is shown here";
+    this.description = `Changes made to this website are logged in this page. 
+    A brief description of each change is located next to its version. Versioning assumes the following:`;
+    this.revisionPolicy = `Minor changes performed to the website`;
+    this.buildPolicy = `Incremental changes`;
+    this.minorPolicy = `Feature releases`;
+    this.majorPolicy = `Major feature releases, or big redesign features`;
+    this.clientChangeLog = `Client Change Log`;
+    this.serverChangeLog = `Server Change Log`;
   }
 
   ngOnInit() {
