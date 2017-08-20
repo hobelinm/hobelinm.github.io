62d6e71e708b449c4e01de13e285a01f4c9d9396
Hugo Belin <hobelinm@gmail.com>
2017-08-20 16:09:28 -0700

    0.1.14.0 - Messaging service, set footer height

--- belin-app/src/app/constants/constants.ts
+++ belin-app/src/app/constants/constants.ts
@@ -8,7 +8,7 @@ import { KeyValuePair } from '../models/keyvaluepair.model';
 const constantData = [
     { 
         key: 'Invariant.Constant.Shared.ShellVersion', 
-        value: '0.1.13.1' 
+        value: '0.1.14.0' 
     },
     { 
         key: 'Invariant.Constant.Shared.Release.CodeName', 
@@ -16,6 +16,10 @@ const constantData = [
     },
 
     // Change Logs:
+    { 
+        key: 'en-US.Constant.ChangeLog.ChangeLog.0.1.14.0', 
+        value: '0.1.14.0 - Messaging service, set footer height' 
+    },
     { 
         key: 'en-US.Constant.ChangeLog.ChangeLog.0.1.13.1', 
         value: '0.1.13.1 - Minor CSS upgrades' 
